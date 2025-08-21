da62b4a238ebb979716290e3d113ebd6283a4d81
José Carlos <carlos.assis.jr@hotmail.com>
2025-08-21 20:14:13 -0300

    save

--- Linguagem_de_Programação_IV/14-08/exemplo1_soma.php
+++ Linguagem_de_Programação_IV/14-08/exemplo1_soma.php
@@ -10,16 +10,15 @@
 
 <body>
   <div class="container">
-    <h1 >Soma</h1>
-    <form method="post" action="exerc_soma.php">
-      <div class="mb-3">
-        <label for="n1" class="form-label">n1</label>
-        <input type="number" id="n1" name="n1" class="form-control">
-      </div>
-      <div class="mb-3">
-        <label for="n2" class="form-label">n2</label>
-        <input type="number" id="n2" name="n2" class="form-control">
-      </div>
+  <h1>Soma de Valores</h1>
+  <form method="post" action="ex1_soma.php">
+    <div class="mb-3">
+              <label for="valor1" class="form-label">Informe o primeiro numero:</label>
+              <input type="number" id="valor1" name="valor1" class="form-control" required="">
+            </div><div class="mb-3">
+              <label for="valor2" class="form-label">Informe o Segundo numero:</label>
+              <input type="text" id="valor2" name="valor2" class="form-control" required="">
+            </div>
       <button type="submit" class="btn btn-primary">Enviar</button>
     </form>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
--- Linguagem_de_Programação_IV/14-08_php/exerc_soma.php
+++
@@ -1,7 +0,0 @@
-<?php
-$n1 = $_POST['n1'];
-$n2 = $_POST['n2'];
-$soma = $n1 + $n2;
-
-echo "<p>soma dos numeros é $soma<p/>";
-?>
