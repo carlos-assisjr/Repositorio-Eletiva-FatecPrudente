85b99aac3be6efe05a1690cb8973d2d34588b090
José Carlos de Assis Jr <carlos.assis.jr@hotmail.com>
2025-08-23 00:05:53 -0300

    save

--- Linguagem_de_Programação_IV/lista_exercicio/exerc13_conversao_cent.php
+++ Linguagem_de_Programação_IV/lista_exercicio/exerc13_conversao_cent.php
@@ -11,7 +11,9 @@
 <body>
     <div class="container">
         <?php
-        
+        $metro = $_POST['metro'];
+        $converte = $metro * 100;
+        echo "<p> O valor de metro em centímetro é: $converte<p>"
         ?>
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
