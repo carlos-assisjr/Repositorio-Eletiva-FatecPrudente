9adecca4e239367ceb749cbe7e1d66f91056de2b
José Carlos de Assis Jr <carlos.assis.jr@hotmail.com>
2025-09-05 10:18:39 -0300

    save

--- Linguagem_de_Programação_IV/lista_exercicios3/exerc4.php
+++ Linguagem_de_Programação_IV/lista_exercicios3/exerc4.php
@@ -4,33 +4,40 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Verifique se a data informada é válida e apresente a data em formato dd/mm/YYYY.</title>
+    <title>Verifique se a data informada é válida e apresente a data em formato dd/mm/YYYY. </title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
     <div class="container py-3">
-        <h1>Verifique se a data informada é válida e apresente a data em formato dd/mm/YYYY.</h1>
+        <h1>Verifique se a data informada é válida e apresente a data em formato dd/mm/YYYY. </h1>
         <form method="post">
             <div class="mb-3">
-                <label for="data" class="form-label">informe uma data:</label>
-                <input type="date" id="data" name="data" class="form-control" required="">
+                <label for="dia" class="form-label">informe o dia:</label>
+                <input type="number" id="dia" name="dia" class="form-control" required="">
+            </div>
+            <div class="mb-3">
+                <label for="mes" class="form-label">informe o mês:</label>
+                <input type="number" id="mes" name="mes" class="form-control" required="">
+            </div>
+            <div class="mb-3">
+                <label for="ano" class="form-label">informe o ano:</label>
+                <input type="number" id="ano" name="ano" class="form-control" required="">
             </div>
             <button type="submit" class="btn btn-primary">Enviar</button>
         </form>
         <?php
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-            $data = $_POST['data'];
-            $dia = date("d");
-            $hora = date("H:i:s");
-            echo "<p>data : $data</p>";
-            echo "<p>dia: $dia</p>";
-            echo "<p>hora: $hora</p>";
-            if (checkdate($data)) {
-                echo "<p>data informada existe</p>";
+            $dia = (int)$_POST['dia'];
+            $mes = (int)$_POST['mes'];
+            $ano = (int)$_POST['ano'];
+
+            if (checkdate($dia, $mes, $ano)) {
+                $data = sprintf("%02d/%02d/%04d", $dia, $mes, $ano);
+                echo "<p>data válida: $data</p>";
             } else {
-                echo "<p>A data informada não existe</p>";
+                echo "<p>Data inválida!</p>";
             }
         }
         ?>
