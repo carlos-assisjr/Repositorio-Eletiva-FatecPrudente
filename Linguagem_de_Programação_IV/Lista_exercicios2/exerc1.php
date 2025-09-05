fb84f1b9efeb2aecabef31ea6fc8a847f8d60659
José Carlos de Assis Jr <carlos.assis.jr@hotmail.com>
2025-09-05 00:41:42 -0300

    save

--- Linguagem_de_Programação_IV/Lista_exercicios2/exerc1.php
+++ Linguagem_de_Programação_IV/Lista_exercicios2/exerc1.php
@@ -89,7 +89,6 @@
             echo "<p> O menor valor informado é: $menor e está na posição $posicao";
         }
         ?>
-        ?>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
     </div>
 </body>
