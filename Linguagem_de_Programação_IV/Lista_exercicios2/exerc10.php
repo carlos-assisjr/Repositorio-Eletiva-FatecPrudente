6cf893efa6536ef16e3a793f5a2e6a0c38e1105e
José Carlos de Assis Jr <carlos.assis.jr@hotmail.com>
2025-09-18 22:53:17 -0300

    save

---
+++ Linguagem_de_Programação_IV/Lista_exercicios2/exerc10.php
@@ -0,0 +1,21 @@
+<?php
+include("cabecalho.php");
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $numero = $_POST['numero'];
+    for ($i = 1; $i <= 10; $i++) {
+        $resultado = $numero * $i;
+        echo "<p>{$numero} x {$i} = {$resultado}</p>";
+    }
+}
+
+?>
+<form method="post">
+    <div class="mb-3">
+        <label for="numero" class="form-label">informe numero:</label>
+        <input type="number" id="numero" name="numero" class="form-control" required="">
+    </div>
+    <button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+<?php
+include("rodape.php");
+?>
