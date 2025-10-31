d4fa92cd1053b39c47242782894d523419a78ece
José Carlos <carlos.assis.jr@hotmail.com>
2025-10-30 21:19:49 -0300

    Aula 30-10

--- Linguagem_de_Programação_IV/projeto/produtos.php
+++ Linguagem_de_Programação_IV/projeto/produtos.php
@@ -1,8 +1,10 @@
 <?php
     require("cabecalho.php");
     require("conexao.php");
+
     try{
-        $stmt = $pdo->query("SELECT c.nome, p* FROM produto p Inner Join categoria c on c.id = p.categoria_id");
+        $stmt = $pdo->query("SELECT c.nome, p.* FROM produto p
+                            INNER JOIN categoria c ON c.id = p.categoria_id");
         $dados = $stmt->fetchAll();
     } catch(\Exception $e){
         echo "Erro: ".$e->getMessage();
@@ -26,14 +28,22 @@
 ?>
 
 <h2>Produtos</h2>
-<a href="novo_produto.php" class="btn btn-success mb-3">Novo Registro</a>
+<a href="novo_produto.php" class="btn btn-success mb-3 no-print">Novo Registro</a>
 <table class="table table-hover table-striped">
     <thead>
+        <tr>
+            <th colspan="3">Dados dos Produtos</th>
+            <th class="no-print">
+                <button class="btn btn-secondary" onclick="window.print()">
+                    Imprimir
+                </button>
+            </th>
+        </tr>
         <tr>
             <th>ID</th>
             <th>Descrição</th>
             <th>Categoria</th>
-            <th>Ações</th>
+            <th class="no-print">Ações</th>
         </tr>
     </thead>
     <tbody>
@@ -44,7 +54,7 @@
             <td><?= $d['id'] ?></td>
             <td><?= $d['descricao'] ?></td>
             <td><?= $d['nome'] ?></td>
-            <td class="d-flex gap-2">
+            <td class="d-flex gap-2 no-print">
                 <a href="editar_produto.php?id=<?= $d['id'] ?>" class="btn btn-sm btn-warning">Editar</a>
                 <a href="consultar_produto.php?id=<?= $d['id'] ?>" class="btn btn-sm btn-info">Consultar</a>
             </td>
